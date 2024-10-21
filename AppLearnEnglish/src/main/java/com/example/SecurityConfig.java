193c68ea44caa804470c86834a39a482f3b6fe3f
luonghoantrongct2004 <luonghoantrongct2004@gmail.com>
2024-10-21 16:28:00 +0700

    Init Project

---
+++ AppLearnEnglish/src/main/java/com/example/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())  // Disable CSRF for stateless sessions
+                .authorizeHttpRequests(auth -> auth  // Use authorizeHttpRequests instead of authorizeRequests
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(logout -> logout.permitAll())  // Allow logout for all users
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // Stateless session management
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
---
+++ AppLearnEnglish/src/main/java/com/example/applearnenglish/AppLearnEnglishApplication.java
@@ -0,0 +1,13 @@
+package com.example.applearnenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLearnEnglishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppLearnEnglishApplication.class, args);
+    }
+
+}
